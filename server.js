eda8b4b177c61309edb3d2001cf84d2819007694
dmitrii <dmitry-orlov@hotmail.com>
2018-05-30 19:10:58 +0200

    adding cookie parsing on landing

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // Used to start the site server
 const express = require('express');
+var cookieParser = require('cookie-parser')
 const path = require('path');
 const serveStatic = require('serve-static');
 
 app = express();
+app.use(cookieParser());
+app.get('/', function (req, res) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+});
 app.use("/", serveStatic(path.join(__dirname, '/dist')))
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {

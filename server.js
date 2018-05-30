0b926354e200cd274862e5ee1c9fd56cdb6ed12c
dmitrii <dmitry-orlov@hotmail.com>
2018-05-30 21:04:04 +0200

    cookie based redirect

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 // Used to start the site server
 const express = require('express');
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const serveStatic = require('serve-static');
+const RedirectService = require('./shared/RedirectService');
 
 app = express();
 app.use(cookieParser());
-app.get('/', function (req, res) {
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
 
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
+// Token cookie control
+app.get('/', function (req, res, next) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies);
+  const tokenCookie = req.cookies.token;
+  console.log(typeof tokenCookie);
+  if (typeof tokenCookie === 'string' && tokenCookie !== '') {
+    const rs = new RedirectService();
+    const url = rs.getUrl('CLIENT_APP', { key: 'token', value: tokenCookie });
+    console.info(`Redirecting to ${url}`);
+    if (url !== '') {
+      res.redirect(url);
+      return;
+    }
+  }
+  next();
+  return;
 });
+
+// 
 app.use("/", serveStatic(path.join(__dirname, '/dist')))
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/dist/index.html');
 })
 
 const port = process.env.PORT || 5000;
--- src/lib/RedirectService.js
+++ src/lib/RedirectService.js
@@ -1,28 +1,7 @@
-export default class RedirectService {
-  constructor() {
-    this.routes = {
-      CLIENT_APP: {
-        base: 'https://volunteero-front-end-base.herokuapp.com/',
-      },
-    };
-  }
+/**
+ * This is a translation module to use 'export'  and 'import' module syntax
+ */
 
-  /**
-   * Returns a route with the query string from internal route table
-   * returns a valid url if found and null if the route with key is not known
-   * @param {String} key
-   * @return {String | null}
-   */
-  getUrl(key, ...queryParams) {
-    const route = this.routes[key];
-    console.log(route);
-    // TODO: use a better existance check
-    if (route) {
-      const queryArray = queryParams.map(q => `${q.key}=${q.value}`);
-      const queryString = (queryArray.length > 0) ? queryArray.join('&') : '';
-      const querySuffix = (queryString === '') ? '' : `?${queryString}`;
-      return `${route.base}${querySuffix}`;
-    }
-    return null;
-  }
-}
+const RedirectService = require('../../shared/RedirectService');
+
+export default RedirectService;
